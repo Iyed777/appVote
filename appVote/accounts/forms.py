0d1e491dec88c001fce37f98a78959c6620d282f
iyed777 <iyedsla@gmail.com>
2017-02-09 12:58:25 +0100

    some changes

--- appVote/accounts/forms.py
+++ appVote/accounts/forms.py
@@ -7,4 +7,4 @@ class subsForm(forms.Form):
 	firt_name = forms.CharField(label="first name")
 	last_name = forms.CharField(label="last name")
 	email = forms.EmailField(label="e-mail")
-	birth_date = forms.DateField(label="birth date")
+	birth_date = forms.DateField(label="birth date", widget=forms.SelectDateWidget(years=range(1970, 2017)))
